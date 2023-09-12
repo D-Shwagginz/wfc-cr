64eb250071d1665a403c3de3423edaeffd7b2667
D. Shwagginz <dshwagbusiness@gmail.com>
2023-09-12 14:55:25 -0700

    chungus

--- rsrc/install.sh
+++ rsrc/install.sh
@@ -1,4 +1,4 @@
 gcc -c -w -fPIC rsrc/wfc.c -o rsrc/wfc.o
-sudo gcc rsrc/wfc.o -shared -o /usr/local/lib/LibWFC.so
-sudo cp /usr/local/lib/LibWFC.so /usr/lib/LibWFC.so
-# sudo ln -s /usr/lib/LibWFC.so /lib/LibWFC.so
+sudo gcc rsrc/wfc.o -shared -o /usr/local/lib/libwfc.so
+sudo cp /usr/local/lib/libwfc.so /usr/lib/libwfc.so
+sudo ln -s /usr/lib/libwfc.so /lib/libwfc.so
